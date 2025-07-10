59a2a42e726dc1a5808dc2cafa7f1cc9dd580a90
mbali-18 <faithfulservent18@gmail.com>
2025-07-11 00:03:33 +0200

    Using Markdown Data For Rendering Posts

--- pages/index.js
+++ pages/index.js
@@ -2,48 +2,23 @@ import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with Next.js",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next.js is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.",
-    date: "2025-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with Next.js",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next.js is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR",
-    date: "2025-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with Next.js",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next.js is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.",
-    date: "2025-02-10",
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with Next.js",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next.js is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.",
-    date: "2025-02-10",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 export default HomePage;
