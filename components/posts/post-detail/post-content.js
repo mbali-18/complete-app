c9e085d53246b627a051aba71479a7dfd74767fa
mbali-18 <faithfulservent18@gmail.com>
2025-07-11 22:14:50 +0200

    working on 10000 errors

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,19 +3,13 @@ import ReactMarkdown from "react-markdown";
 import Postheader from "./post-header";
 import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with Next.js",
-  image: "getting-started-nextjs.png",
-  content: "# This is a first",
-  date: "2025-02-10",
-};
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/props/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <Postheader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <Postheader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,32 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostFiles, getPostData } from "../../lib/posts-util";
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
 }
 
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  // Convert slug to filename
+  const postFileName = `${slug}.md`;
+  const postData = getPostData(postFileName);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 60,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostFiles();
+  const slugs = postFilenames.map((filename) => filename.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false, // can also be true or 'blocking'
+  };
+}
 export default PostDetailPage;
