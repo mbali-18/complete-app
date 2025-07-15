d9cfdab06fec3b2b5d8dc38cb0e472c6bf0c1c78
mbali-18 <faithfulservent18@gmail.com>
2025-07-15 14:35:20 +0200

    Adding A "_document.js" File

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import { Fragment } from "react";
+import Head from "next/head";
 
-import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
 import { getFeaturedPosts } from "../lib/posts-util";
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Mbali's Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
@@ -15,10 +23,12 @@ function HomePage(props) {
 
 export function getStaticProps() {
   const featuredPosts = getFeaturedPosts();
+
   return {
     props: {
       posts: featuredPosts,
     },
   };
 }
+
 export default HomePage;
