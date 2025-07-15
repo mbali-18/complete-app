a7d6902740f7b66fb243d60a4431642612de412e
mbali-18 <faithfulservent18@gmail.com>
2025-07-15 12:33:12 +0200

    Preparing The Contact Form

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,10 +1,11 @@
-import PostGrid from "../posts/posts-grid";
+import PostsGrid from "../posts/posts-grid";
 import classes from "./featured-posts.module.css";
+
 function FeaturedPosts(props) {
   return (
     <section className={classes.latest}>
       <h2>Featured Posts</h2>
-      <PostGrid posts={props.posts} />
+      <PostsGrid posts={props.posts} />
     </section>
   );
 }
